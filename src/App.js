e026700863fcb3c166bc3daa2741a7c1f056ae83
Rammah <ramahnore@gmail.com>
2022-05-04 13:57:33 +0200

    Fetching data from the Wordpress API

--- src/App.js
+++ src/App.js
@@ -20,6 +20,19 @@ function App() {
       </header>
     </div>
   );
+	const [data, setData] = useState();
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		fetch(
+			"https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json"
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				setData(data);
+				console.log(data);
+				setLoading(false);
+			});
+	}, []);
 }
 
 export default App;
